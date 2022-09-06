73dbd74033c5e28467b26b785985362d6f984249
Nathaniel Astudillo <nathanielastudillo@gmail.com>
2022-09-05 23:25:05 -0400

    working

--- code.js
+++ code.js
@@ -11,29 +11,47 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 figma.showUI(__html__);
 function main(msg) {
     return __awaiter(this, void 0, void 0, function* () {
+        function createTweetFrame(inputText, arr) {
+            // *** START FUNCTION ***
+            // create frame with black background
+            const frame = figma.createFrame();
+            frame.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
+            // set frame layout to center text
+            frame.layoutMode = 'VERTICAL';
+            frame.primaryAxisAlignItems = 'CENTER';
+            frame.counterAxisAlignItems = 'CENTER';
+            // resize to counter the resize apparently triggered by setting layoutMode
+            frame.resize(1080, 1080);
+            frame.paddingLeft = 40;
+            frame.paddingRight = 40;
+            frame.paddingTop = 40;
+            frame.paddingBottom = 40;
+            //create white text and center + set width
+            const text = figma.createText();
+            text.characters = inputText;
+            text.fontSize = 64;
+            //text.textAutoResize = 'HEIGHT';
+            text.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
+            text.resize(900, text.height);
+            //add text as child of parent via. frame.appendChild()
+            frame.appendChild(text);
+            frame.name = 'tweet_img_' + arr;
+            // *** END FUNCTION ***
+        }
         // wait for fonts
         yield figma.loadFontAsync({ family: "Inter", style: "Regular" });
         console.log(msg.tweetText);
-        // eventually need to handle the case where msg is a long string of comma separated tweets 
-        // might make more sense to handle this in the UI and let users paste multiple tweets into multiple text boxes 
-        // *** START FUNCTION ***
-        // create frame with black background
-        const frame = figma.createFrame();
-        frame.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-        // set frame layout to center text
-        frame.layoutMode = 'VERTICAL';
-        frame.primaryAxisAlignItems = 'CENTER';
-        frame.counterAxisAlignItems = 'CENTER';
-        // resize to counter the resize apparently triggered by setting layoutMode
-        frame.resize(1080, 1080);
-        //create white text and center
-        const text = figma.createText();
-        text.characters = msg.tweetText;
-        text.fontSize = 64;
-        text.fills = [{ type: 'SOLID', color: { r: 1, g: 1, b: 1 } }];
-        //add text as child of parent via. frame.appendChild()
-        frame.appendChild(text);
-        // *** END FUNCTION ***
+        // split tweetText by tweet
+        const tweetArray = msg.tweetText.split('%|%');
+        tweetArray.map((x) => x.trim());
+        if (tweetArray.length == 1) {
+            createTweetFrame(msg.tweetText.trim(), 0);
+        }
+        else {
+            for (let [index, tweet] of tweetArray.entries()) {
+                createTweetFrame(tweet, index);
+            }
+        }
         figma.closePlugin();
     });
 }
